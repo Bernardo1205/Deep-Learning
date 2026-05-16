8273b546409c5d2b2e35f28e252a13bd70343f65
bernanrdo <bernardo.llata@opendeusto.es>
2026-05-16 13:58:44 +0200

    Assigment 3 bernado llata

---
+++ src/assignment-3/data.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import pandas as pd
+
+DATASET_DIR = Path("dataset")
+
+def load(csv_name):
+    return  pd.read_csv(DATASET_DIR / csv_name , sep="," , on_bad_lines='skip')
+
+
