efe0f3dc5ab851ab25c21a82f29a7e8320e4c3a0
bernanrdo <bernardo.llata@opendeusto.es>
2026-05-16 14:18:29 +0200

    Assigment 3 bernado llata

--- src/assignement-3/benardollata/data.py
+++ src/assignement-3/benardollata/data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 
-DATASET_DIR = Path("dataset")
+DATASET_DIR = Path("benardollata/dataset")
 
 def load(csv_name):
     return  pd.read_csv(DATASET_DIR / csv_name , sep="," , on_bad_lines='skip')
